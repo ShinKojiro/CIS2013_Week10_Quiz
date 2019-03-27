d6f0e58ed2def58e2a4b1ac9f0cd81b8f8ae2c0d
ShinKojiro <42621442+ShinKojiro@users.noreply.github.com>
2019-03-27 17:52:01 -0500

    start

---
+++ mail.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+using namespace std;
+
+int main(){
+    char next;
+    string sentence;
+    int bCount;
+
+    cout << "Type a sentence, and be sure to include lots of \"b\"'s " << endl;
+    cin >> sentence;
+
+    
+}
